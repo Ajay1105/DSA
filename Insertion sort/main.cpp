7f5eb9193e67341563ebecad7be41f66fe7d3936
Ajay <aky11052003@gmail.com>
2022-03-11 18:09:26 +0530

    110322

--- Insertion sort/main.cpp
+++ Insertion sort/main.cpp
@@ -8,7 +8,7 @@ for (int i =0;i<n;i++){
 }cout<<endl;
 }
 
-void bubblesort(int * arr, int n){
+void insertionsort(int * arr, int n){
 for(int i=1;i<n;i++){
     int j=i-1,key = arr[i];
     while(j>0 &&arr[j]<key){
@@ -22,7 +22,7 @@ int main()
 { int arr[] = {8,2,3,5};
     cout << "Unsorted array" << endl;
     print(arr,4);
-    bubblesort(arr,4);
+    insertionsort(arr,4);
     print(arr,4);
 
     return 0;
--- Linked List/main.c
+++ Linked List/main.c
@@ -13,8 +13,9 @@ insert->data =e;
 insert->next = ptr;
 insert->back = NULL;
 ptr->back = insert;
+printf("%d is inserted at the start of linked list\n",e);
 return insert;
-};
+}
 
 struct Node* Insert_end(struct Node*ptr,int e){
 struct Node * insert = (struct Node*) malloc(sizeof(struct Node));
@@ -22,8 +23,9 @@ insert->data =e;
 insert->next = NULL;
 insert->back = ptr;
 ptr->next = insert;
+printf("%d is inserted at the end of linked list\n",e);
 return insert;
-};
+}
 
 void Insert_in_between(struct Node * ptr,struct Node * ptr2,int e){
 struct Node * insert = (struct Node*) malloc(sizeof(struct Node));
@@ -31,6 +33,7 @@ insert->data =e;
 ptr->next =insert;
 insert->next = ptr2;
 insert->back = ptr;
+printf("%d is inserted in between %d and %d in linked list\n",e,ptr->data,ptr2->data);
 }
 
 void display(struct Node * ptr){
